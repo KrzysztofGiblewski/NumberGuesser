c0323b964fd2c31780085d700c4accc208031b03
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2021-03-01 20:14:24 +0100

    kolejne kroki

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,14 +6,36 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
+            string imie;
+           imie= PobierzImie();
+            WyswietlTekst("Cześć "+imie);
+            WyswietlTekst("a teraz wylosóje liczbę którą masz zgadnąć");
+            int wylosowanaLiczba = Losoj();
 
-            int wylosowana = random.Next(0, 10);
-            Console.WriteLine("wylosowano "+wylosowana);
-
-          
+            WyswietlTekst("wylosowano");
+            string liczba = wylosowanaLiczba.ToString();
+            WyswietlTekst(liczba);
+            WyswietlTekst($"kolejną {imie} zgadniesz ty");
         }
 
-        
+        static void WyswietlTekst(string tekst)
+        {
+            Console.WriteLine( tekst);
+
+        }
+
+        static string PobierzImie()
+        {
+            Console.WriteLine("podaj swoje imię");
+            string imie = Console.ReadLine();
+            return imie;
+        }
+        static int Losoj()
+        {
+            Random random = new Random();
+            int wylosowana = random.Next(0, 10);
+            return wylosowana;
+        }
+
     }
 }
